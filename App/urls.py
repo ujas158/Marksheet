eacb526d6f2e85fb2b33d7d8067a5b678c6c464b
ujas158 <ujas158@gmail.com>
2023-09-14 10:03:37 +0530

    commiting all files to save yet changes are pending

---
+++ App/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from App import views
+
+
+urlpatterns = [
+    path('', views.index, name="home"),
+    path('login/', views.login, name="login"),
+    path('logout/', views.logout, name="logout"),
+
+]
